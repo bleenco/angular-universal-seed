c4f19db31efec9e67655b6f6ecd4593cf2a9420b
Jan Kuri <jkuri88@gmail.com>
2017-04-19 05:38:15 +0200

    chore(release): 0.3.0

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -11,23 +11,4 @@ describe('App', () => {
     let result  = 'Angular Webpack Seed';
     expect(subject).toEqual(result as any);
   });
-
-  // it('should have header', () => {
-  //   let subject = element(by.css('h1')).isPresent();
-  //   let result  = true;
-  //   expect(subject).toEqual(result);
-  // });
-
-  // it('should have <home>', () => {
-  //   let subject = element(by.css('app home')).isPresent();
-  //   let result  = true;
-  //   expect(subject).toEqual(result);
-  // });
-
-  // it('should have buttons', () => {
-  //   let subject = element(by.css('button')).getText();
-  //   let result  = 'Submit Value';
-  //   expect(subject).toEqual(result);
-  // });
-
 });
